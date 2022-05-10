0e01d4e1e6878adfef18be5fabc1eeec83bb6958
nikitashevchenkodp <nrcnekita9@gmail.com>
2022-05-10 12:37:29 +0300

    fixed slider, style of js code

--- js/main.js
+++ js/main.js
@@ -1,83 +1,94 @@
 
 const fixedHeader = () => {
 
-  let header = document.getElementById('header')
-  let main = document.getElementById('main')
+  const header = document.getElementById('header');
+  const main = document.getElementById('main');
 
   window.addEventListener('scroll', () => {
-      let scrollPosition = this.window.scrollY
-      let mainHeight = main.scrollHeight
+      const scrollPosition = this.window.scrollY;
+      const mainHeight = main.scrollHeight;
 
-      if(scrollPosition > mainHeight) {
-        header.classList.add('fixed')
+      if( scrollPosition > mainHeight ) {
+        header.classList.add('fixed');
       } else {
-        header.classList.remove('fixed')
+        header.classList.remove('fixed');
       }
-    })
-}
+    });
+};
 
 /*Smooth scrol */
 
 const smoothScroll = () => {
   document.addEventListener('click', (event) => {
-    event.preventDefault()
-    let id = event.target.dataset.scroll
-    if(!id) return
-    const elemPosition = document.getElementById(id).offsetTop
+    event.preventDefault();
+    const id = event.target.dataset.scroll;
+    if( !id ) return
+    const elemPosition = document.getElementById(id).offsetTop;
     window.scrollTo({
       top: elemPosition - 50,
       behavior: 'smooth'
-    })
-    let nav = document.getElementById('header__navigation')
-    nav.classList.remove('show')
-  })
-}
+    });
+    const nav = document.getElementById('header__navigation');
+    nav.classList.remove('show');
+  });
+};
 
 /*Toggle NavBar */
 
 const toggleNavigation = () => {
   document.getElementById('toggleNav').addEventListener('click', () => {
-    let nav = document.getElementById('header__navigation')
-    nav.classList.toggle('show')
+    const nav = document.getElementById('header__navigation');
+    nav.classList.toggle('show');
   })
 }
 
 /*Slider */
 
 const charactersSlider = () => {
-  let items = document.querySelectorAll('.select__character')
+  const items = document.querySelectorAll('.select__character');
+
   for(let item of items) {
     item.addEventListener('click', (e) => {
-      document.querySelector('.characters__info__select .active').classList.remove('active')
-      const item = e.target.closest('.select__character')
-      item.classList.add('active')
-      let id = item.getAttribute("id")
-      document.querySelector('.characters__image__img').setAttribute('src', `/img/slider/${id}`)
-    })
-    
-  }
-  
-    
-}
+      document.querySelector('.characters__info__select .active').classList.remove('active');
+      const item = e.target.closest('.select__character');
+      item.classList.add('active');
+      let id = item.getAttribute("id");
+      document.querySelector('.characters__image__img').setAttribute('src', `./img/slider/${id}`);
+    });
+  };
+};
 
 /*Go Up Btn Visible */
 
 const goUpBtn = () => {
   window.addEventListener('scroll', () => {
-    const elem = document.querySelector('.benefits')
-    const elemPos = elem.getBoundingClientRect().top
-    if(elemPos < 0) {
-      document.querySelector('.go__up').classList.add('go__show')
+
+    const elem = document.querySelector('.benefits');
+    const elemPos = elem.getBoundingClientRect().top;
+
+    if( elemPos < 0 ) {
+      document.querySelector('.go__up').classList.add('go__show');
     } else {
-      document.querySelector('.go__up').classList.remove('go__show')
+      document.querySelector('.go__up').classList.remove('go__show');
     }
-  })
-  
+  });
+
+};
+
+
+smoothScroll();
+fixedHeader();
+toggleNavigation();
+charactersSlider();
+goUpBtn();
+
+const obj1 = {
+  a: 1,
+  b: 2,
+  c: 3
 }
 
+const obj2 = {...obj1, d:4}
 
-smoothScroll()
-fixedHeader()
-toggleNavigation()
-charactersSlider()
-goUpBtn()
+console.log(obj1)
+console.log(obj2)
